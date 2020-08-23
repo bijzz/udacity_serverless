244c6b9b7be5541ab825e6382a2ca3c82ec112a8
kern <no@mail.com>
2020-08-23 18:16:43 +0200

    Final adjustment

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '4c4kr4myd3'
+const apiId = 'ki0d64tq5a'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
